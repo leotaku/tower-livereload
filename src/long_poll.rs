79d03f66d15843e629581bc9ac52e19c58d9e55c
Leo Gaskin <leo.gaskin@le0.gs>
2022-11-14 06:53:01 +0100

    Simplify long-poll implementation and require fewer copies

--- src/long_poll.rs
+++ src/long_poll.rs
@@ -1,13 +1,15 @@
-use std::{convert::Infallible, future::Future, pin::Pin, task::Poll};
+use std::{convert::Infallible, task::Poll};
 use tokio::sync::broadcast::Receiver;
 
 pub struct LongPollBody {
-    receiver: Receiver<()>,
+    receiver: Option<Receiver<()>>,
 }
 
 impl LongPollBody {
     pub fn new(receiver: Receiver<()>) -> Self {
-        LongPollBody { receiver }
+        LongPollBody {
+            receiver: Some(receiver),
+        }
     }
 }
 
@@ -19,22 +21,16 @@ impl http_body::Body for LongPollBody {
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<Option<Result<Self::Data, Self::Error>>> {
-        let polled = {
-            let mut boxed = Box::pin(self.receiver.recv());
-            Pin::new(&mut boxed).poll(cx)
-        };
-
-        match polled {
-            Poll::Ready(Ok(_)) => Poll::Ready(None),
-            Poll::Ready(Err(_)) | Poll::Pending => {
+        match self.receiver.take() {
+            Some(mut receiver) => {
                 let waker = cx.waker().clone();
-                let mut receiver = self.receiver.resubscribe();
                 tokio::spawn(async move {
                     receiver.recv().await.ok();
                     waker.wake();
                 });
                 Poll::Pending
             }
+            None => Poll::Ready(None),
         }
     }
 
