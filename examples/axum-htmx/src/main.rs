a4b8a1be32077bc15bbf691dde19206744771346
Leo Gaskin <leo.gaskin@le0.gs>
2023-11-22 16:50:13 +0100

    Commit "axum-htmx" example

---
+++ examples/axum-htmx/src/main.rs
@@ -0,0 +1,27 @@
+use axum::{http::Request, Router};
+use notify::Watcher;
+use std::path::Path;
+use tower_http::services::ServeDir;
+use tower_livereload::LiveReloadLayer;
+
+fn not_htmx_predicate<T>(req: &Request<T>) -> bool {
+    !req.headers().contains_key("hx-request")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let livereload = LiveReloadLayer::new();
+    let reloader = livereload.reloader();
+    let app = Router::new()
+        .nest_service("/", ServeDir::new(Path::new("assets")))
+        .layer(livereload.request_predicate(not_htmx_predicate));
+
+    let mut watcher = notify::recommended_watcher(move |_| reloader.reload())?;
+    watcher.watch(Path::new("assets"), notify::RecursiveMode::Recursive)?;
+
+    axum::Server::bind(&"0.0.0.0:3030".parse()?)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
