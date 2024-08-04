625ed48c92d2b2e32716b586a81954d1fa141920
Leo Gaskin <leo.gaskin@le0.gs>
2024-08-04 16:28:24 +0200

    Commit some actual tests

--- examples/playwright/tests/simple.spec.ts
+++ examples/playwright/tests/simple.spec.ts
@@ -8,3 +8,23 @@ test("has heading", async ({ page }) => {
     page.getByRole("heading", { name: "Playwright!" }),
   ).toBeVisible();
 });
+
+test("reload", async ({ page, request }) => {
+  await page.goto("/");
+
+  // Wait for load event on this page.
+  const reload = page.waitForRequest("/");
+
+  // Trigger a reload.
+  await request.post("/reload");
+
+  // Ensure reload actually happened.
+  await reload;
+});
+
+test.fail("no reload", async ({ page }) => {
+  await page.goto("/");
+
+  // Ensure no reload happens when not triggered.
+  await page.waitForRequest("/", { timeout: 100 });
+});
