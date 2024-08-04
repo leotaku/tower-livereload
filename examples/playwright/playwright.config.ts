5e271e620399d8b8c8fe72d1ce932d08596a4545
Leo Gaskin <leo.gaskin@le0.gs>
2024-08-04 16:28:10 +0200

    Disable parallel tests for now

--- examples/playwright/playwright.config.ts
+++ examples/playwright/playwright.config.ts
@@ -7,14 +7,13 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./tests",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
+  /* Opt out of parallel tests. */
+  fullyParallel: false,
+  workers: 1,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html", { open: "never" }]],
 
