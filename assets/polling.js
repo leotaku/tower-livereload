2a73bb7366dd666c5314a8cf001eb8457eb82ae7
Leo Gaskin <leo.gaskin@le0.gs>
2024-11-30 13:18:18 +0100

    Merge some Promise.then calls

--- assets/polling.js
+++ assets/polling.js
@@ -12,21 +12,22 @@
     const controller = new AbortController();
     setTimeout(() => controller.abort(), 500);
     fetch(url, { cache: "no-store", signal: controller.signal })
-      .then((resp) => (resp.ok ? resp : Promise.reject()))
-      .then(() => console.log("[tower-livereload] reload..."))
-      .then(() => window.location.reload())
+      .then((resp) => {
+        if (!resp.ok) return Promise.reject();
+        console.log("[tower-livereload] reload...");
+        window.location.reload();
+      })
       .catch(() => setTimeout(() => retry(url), inputs.reloadInterval));
   };
 
   const main = () =>
-    fetch(inputs.longPoll, {
-      cache: "no-store",
-      signal: controller.signal,
-    })
+    fetch(inputs.longPoll, { cache: "no-store", signal: controller.signal })
       .then((rsp) => rsp.text())
       .catch(() => null)
-      .then(() => console.log("[tower-livereload] disconnected..."))
-      .then(() => (unloaded ? null : retry(inputs.backUp)));
+      .then(() => {
+        console.log("[tower-livereload] disconnected...");
+        if (!unloaded) retry(inputs.backUp);
+      });
 
   console.log("[tower-livereload] connected...");
   main();
