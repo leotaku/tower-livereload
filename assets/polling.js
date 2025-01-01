4ce6822b2af0a677b429c20f882d3145b416a393
Leo Gaskin <leo.gaskin@le0.gs>
2025-01-01 11:24:35 +0100

    Make use of pageshow event

--- assets/polling.js
+++ assets/polling.js
@@ -1,13 +1,6 @@
 (() => {
   const inputs = document.currentScript.dataset;
 
-  const controller = new AbortController();
-  var unloaded = false;
-  addEventListener("beforeunload", () => {
-    unloaded = true;
-    controller.abort();
-  });
-
   const retry = (url) => {
     const controller = new AbortController();
     setTimeout(() => controller.abort(), 500);
@@ -20,12 +13,21 @@
       .catch(() => setTimeout(() => retry(url), inputs.reloadInterval));
   };
 
-  console.log("[tower-livereload] connected...");
-  fetch(inputs.longPoll, { cache: "no-store", signal: controller.signal })
-    .then((rsp) => rsp.text())
-    .catch(() => null)
-    .then(() => {
-      console.log("[tower-livereload] disconnected...");
-      if (!unloaded) retry(inputs.backUp);
+  addEventListener("pageshow", () => {
+    const controller = new AbortController();
+    var unloaded = false;
+    addEventListener("beforeunload", () => {
+      unloaded = true;
+      controller.abort();
     });
+
+    console.log("[tower-livereload] connected...");
+    fetch(inputs.longPoll, { cache: "no-store", signal: controller.signal })
+      .then((rsp) => rsp.text())
+      .catch(() => null)
+      .then(() => {
+        console.log("[tower-livereload] disconnected...");
+        if (!unloaded) retry(inputs.backUp);
+      });
+  });
 })();
