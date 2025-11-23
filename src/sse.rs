5d4bc1e728fe7275519ff6846295afb2c9d5abdc
Leo Gaskin <leo.gaskin@le0.gs>
2025-11-23 12:31:14 +0100

    Use tokio notify instead of broadcast channel
    The reason for this change is the fact that `Notify` provides a
    concrete, owned Future type that can easily be stored in our state
    machine, while the `broadcast::Receiver` does not.
    
    This allows us to directly poll the `Notified` future in the
    `poll_frame` function, instead of our previous strategy of spawning a
    tasks that wakes our future whenever the channel receives a value.
    
    This is important as, in some cases, executors will re-poll a future
    that returned `Poll::Pending` before the waker is called. For example,
    in #30 the the addition of rustls for HTTPS caused pending futures to
    always be polled a second time, triggering a bug.
    
    Also, this change allows us to not require the tokio "rt" feature for
    this crate, which is nice.

--- src/sse.rs
+++ src/sse.rs
@@ -1,7 +1,7 @@
-use std::{convert::Infallible, task::Poll, time::Duration};
+use std::{convert::Infallible, future::Future, pin::Pin, sync::Arc, task::Poll, time::Duration};
 
 use http_body::Frame;
-use tokio::sync::broadcast::Receiver;
+use tokio::sync::{futures::OwnedNotified, Notify};
 
 pub struct ReloadEventsBody {
     state: State,
@@ -9,14 +9,13 @@ pub struct ReloadEventsBody {
 }
 
 enum State {
-    Initial(Receiver<()>),
-    BeforePending(Receiver<()>),
-    Pending,
+    Initial(Arc<Notify>),
+    Pending(Pin<Box<OwnedNotified>>),
     Final,
 }
 
 impl ReloadEventsBody {
-    pub fn new(receiver: Receiver<()>, retry_duration: Duration) -> Self {
+    pub fn new(receiver: Arc<Notify>, retry_duration: Duration) -> Self {
         Self {
             state: State::Initial(receiver),
             retry_duration,
@@ -33,26 +32,19 @@ impl http_body::Body for ReloadEventsBody {
         cx: &mut std::task::Context<'_>,
     ) -> Poll<Option<Result<Frame<Self::Data>, Self::Error>>> {
         match std::mem::replace(&mut self.state, State::Final) {
-            State::Initial(receiver) => {
-                self.state = State::BeforePending(receiver);
+            State::Initial(notify) => {
+                self.state = State::Pending(Box::pin(notify.notified_owned()));
 
                 Poll::Ready(Some(Ok(Frame::data(bytes::Bytes::from_owner(format!(
                     "event: init\ndata:\nretry: {}\n\n",
                     self.retry_duration.as_millis()
                 ))))))
             }
-            State::BeforePending(mut receiver) => {
-                self.state = State::Pending;
-
-                let waker = cx.waker().clone();
-                tokio::spawn(async move {
-                    receiver.recv().await.ok();
-                    waker.wake();
-                });
-                Poll::Pending
-            }
-            State::Pending => {
-                self.state = State::Final;
+            State::Pending(mut notified) => {
+                if notified.as_mut().poll(cx) == Poll::Pending {
+                    self.state = State::Pending(notified);
+                    return Poll::Pending;
+                }
 
                 Poll::Ready(Some(Ok(Frame::data(bytes::Bytes::from_static(
                     b"event: reload\ndata:\n\n",
